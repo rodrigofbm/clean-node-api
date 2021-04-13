84a8cbe49986a998aae2fc3f86a852a3ff832c79
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-04-12 21:02:00 -0300

    test: ensure SignUpController calls EmailValidator with correct e-mail

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,7 @@
 import { EmailValidator } from "../protocols/email-validator";
 import { MissingParamError } from "../errors/missing-param-error";
 import { SignUpController } from "./signup";
+import { InvalidParamError } from "../errors/invalid-param-error";
 
 interface SutTypes {
   sut: SignUpController;
@@ -91,7 +92,7 @@ describe("SignUp Controller", () => {
     const httpRequest = {
       body: {
         name: "any_name",
-        email: "any_email@mail.com",
+        email: "invalid_email@mail.com",
         password: "any_password",
         passwordConfirmation: "any_password",
       },
@@ -99,5 +100,22 @@ describe("SignUp Controller", () => {
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError("email"));
+  });
+
+  test("should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    sut.handle(httpRequest);
+
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
