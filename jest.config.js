3aec0c5dff2dfa47d61e0139da5f6812a4d0e398
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2021-04-12 22:56:19 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   //An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [ '<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**' ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
+  coverageProvider: "babel",
   // The test environment that will be used for testing
   testEnvironment: "node",
 
